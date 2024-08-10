55f19007084386aca99556e4b315babda05f1cf9
MenatalhHossamalden <146476199+MenaTalhHossamAlden@users.noreply.github.com>
2024-08-10 11:10:23 +0300

    Implementing the game

--- Kirby/src/main.js
+++ Kirby/src/main.js
@@ -72,14 +72,72 @@ k.scene('start', async () => {
   k.onKeyPress('space', goToGame);
   k.onGamepadButtonPress('south', goToGame);
   await saveSystem.load();
-  if(!saveSystem.data.maxScore) {
-    saveSystem.data.maxScore = 0
+  if (!saveSystem.data.maxScore) {
+    saveSystem.data.maxScore = 0;
     await saveSystem.save();
   }
 });
 
 k.scene('main', async () => {
-  
+  makeBackground(k);
+  let score = 0;
+  const colliders = await (await fetch('./collidersData.json')).json();
+  const collidersData = colliders.data;
+  k.setGravity(2500);
+  const map = k.add([
+    k.sprite('background'),
+    k.pos(0, -50),
+    k.scale(SCALE_FACTOR),
+  ]);
+  const clouds = map.add([k.sprite('clouds'), k.pos(), { speed: 5 }]);
+  clouds.onUpdate(() => {
+    clouds.move(clouds.speed, 0);
+    console.log(clouds.pos.x);
+    if (clouds.pos.x > 700) {
+      clouds.pos.x = -500; // put the clouds far back so it scrolls again
+    }
+  });
+  const platforms = map.add([
+    k.sprite('obstacles'),
+    k.pos(),
+    k.area(),
+    { speed: 100 },
+  ]);
+  platforms.onUpdate(() => {
+    platforms.move(-platforms.speed, 0);
+    if (platforms.pos.x < -490) {
+      platforms.pos.x = 300; // put the platforms far back so it scrolls again
+      platforms.speed += 30; // progressively increase speed
+    }
+  });
+  k.loop(1, () => {
+    score += 1;
+  });
+  for (const collider of collidersData) {
+    platforms.add([
+      k.area({
+        shape: new k.Rect(k.vec2(0), collider.width, collider.height),
+      }),
+      k.body({ isStatic: true }),
+      k.pos(collider.x, collider.y),
+      'obstacles',
+    ]);
+  }
+  k.add([k.rect(k.width, 50), k.pos(0, -100), k.area(), 'obstacle']);
+  k.add([k.rect(k.width, 50), k.pos(0, 1000), k.area(), 'obstacle']);
+  const player = k.add(makePlayer(k));
+  player.pos = k.vec2(500, 250);
+  player.setControls();
+  player.onCollide('obstacles', () => {
+    if (player.isDead) {
+      return;
+    }
+    k.play('hurt');
+    platforms.speed = 0;
+    player.disableControls();
+    // Create ScoreBox
+    player.isDead = true;
+  });
 });
 
 k.go('start');
